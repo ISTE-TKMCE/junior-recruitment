5aaaf4a9a52b498a98ed63592067ab057428a398
P K Aravindkrishnan <65278075+Aravind-pk@users.noreply.github.com>
2021-09-23 14:38:09 +0530

    init4

---
+++ index.php
@@ -0,0 +1,299 @@
+<?php
+session_start();
+if(isset($_POST['submit']))
+{
+    // echo "<script type='text/javascript'>alert('Registration Temporarily suspended.Thankyou!');</script>";
+    echo "<script type='text/javascript'> document.location = 'home.php'; </script>";
+}
+?>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" href="css/trial.css" />
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/fileinput.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <link rel="stylesheet" href="css/dataTables.bootstrap.min.css" />
+    <link rel="stylesheet" href="css/bootstrap-social.css" />
+    <link rel="stylesheet" href="css/bootstrap-select.css" />
+    <link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css" />
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,500;1,600;1,700&display=swap" rel="stylesheet"> 
+   <!--<script>alert("Registration Closed \nFor more details Contact Vishnu B Vinod (8547816261) " )</script>-->
+    <title>Recruitment | ISTE JUNIOR EXECOM</title>
+  </head>
+<body style="font-family: 'Montserrat', sans-serif;">
+  <?php include('includes/header.php');?>
+  <section style="color: white; background-image:url(images/bg.png);">
+  <center><div class="mt-5" style="display: table-cell;text-align:center;width:100%;vertical-align: middle;min-height:10em;height: 100vh;">
+      <h1 class="pt-5 text-center mx-auto " style="font-size: 40px;">ISTE Execom Registration 2021<br></h1>
+      <p class="mt-5 h1-responsive" style="font-size:30px">Last Date - September 28, 2021</p>
+    </div></center>
+  </section>
+    <div id="cards_landscape_wrap-2">
+      <div class="container">
+          <h1 class="text-center">Posts</h1>
+        <div class="row">
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+
+              <div class="card-flyer">
+                <div class="text-box">
+                  <div class="text-container">
+                    <h6>Documentation</h6>
+                    <div class="text">
+                    <p class="">
+                      You shall be responsible for making write-ups and putting
+                      together reports of all the important events conducted,
+                      meetings held and so on. You will also be in charge of
+                      writing letters requesting permission to access perhaps
+                      certain facilities of the college or to pick a day of
+                      least resistance to present an event. You will handle all
+                      personalized reports of club activities and shall assist
+                      the documentation head. If you consider your pen your
+                      strongest weapon, apply for this post!
+                    </p>
+</div>
+                  </div>
+                </div>
+              </div>
+
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+
+              <div class="card-flyer">
+                <div class="text-box">
+                  <div class="text-container">
+                    <h6>Student Representative</h6>
+                    <div class="text">
+                    <p class="">
+                      You are going to be one of the faces of ISTE. You will be
+                      coordinating all members from each year and will be the
+                      mutual link between the excom and the members. Collecting
+                      opinions issues and criticisms of the members and
+                      communicating with them accordingly will be your part of
+                      the package. You will lead the ISTE class reps and will be
+                      assisting the senior rep head. If you think you got the
+                      leadership skills to take up this post, what are you
+                      waiting for?!
+                    </p>
+</div>
+                  </div>
+                </div>
+              </div>
+
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+
+              <div class="card-flyer">
+                <div class="text-box">
+                  <div class="text-container">
+                    <h6>Public Relations</h6>
+                    <div class="text">
+                    <p class="">
+                      You are the captain of communications. It will be your job
+                      to steer and maintain all of ISTE's social media
+                      communications. You shall be the brains behind the
+                      marketing strategies for events and will be responsible
+                      for website contact forms. All plans for promoting our
+                      programs including checking the status of the requested
+                      permission for conducting the program will be looked on by
+                      you. You will assist the Public Relations Head as well.
+                      The power to expand our reach is merely within your
+                      fingertips. If you think you are social media influencer
+                      enough, apply for this position now!
+                    </p>
+</div>
+                  </div>
+                </div>
+              </div>
+
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+
+              <div class="card-flyer">
+                <div class="text-box">
+                  <div class="text-container">
+                    <h6>Co-designer</h6>
+                    <div class="text">
+                    <p class="">
+                      You are to be the paint to a painting. It is your duty to
+                      create effective posters and attractive promo videos and
+                      arrange the tidbits of the message to be conveyed in the
+                      most constructive way. You will be the head of the ISTE
+                      design team and will guide your team with a dash of
+                      resilience and a pinch of imagination. If you can think
+                      outside the box to impress the audience, sign right up!
+                    </p>
+</div>
+                  </div>
+                </div>
+              </div>
+
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+
+              <div class="card-flyer">
+                <div class="text-box">
+                  <div class="text-container">
+                    <h6>Program Coordinator</h6>
+                    <div class="text">
+                    <p class="">
+                      You are that 'green room guy'. Although you wouldn't be
+                      having a headset to give your orders through, you will
+                      help in the conduction and organization of events. You may
+                      pitch in innovative ideas for events and obtain
+                      permissions for these events to be conducted. You will
+                      also be assisting the event coordinators. If you are that
+                      guy who can handle and manage any situation, come right
+                      through!
+                    </p>
+</div>
+                  </div>
+                </div>
+              </div>
+
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+
+              <div class="card-flyer">
+                <div class="text-box">
+                  <div class="text-container">
+                    <h6>Finance Manager</h6>
+                    <div class="text">
+                    <p class="">
+                      You will be dealing with the financial faction of ISTE and
+                      shall handle our budget feasibly and responsibly.You are
+                      in charge of collecting and approving new registrations.
+                      The collection of registration fees will also be under
+                      your jurisdiction. You will be assisting the financial
+                      head. If your calculations never falter, this is the post
+                      for you!
+                    </p>
+</div>
+                  </div>
+                </div>
+              </div>
+
+          </div>
+
+          
+          
+          
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+
+          <div class="card-flyer">
+            <div class="text-box">
+              <div class="text-container">
+                <h6>Registration</h6>
+                <div class="text">
+                  <p class="">
+                    You are the beginning to a successful program. It will be
+                    your duty to handle the registration process of all
+                    programs. You will be assigned to the technical side of
+                    the registration process and will analyse and correct the
+                    faults in the process. You will also help the registration
+                    head.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+
+          <div class="card-flyer">
+            <div class="text-box">
+              <div class="text-container">
+                <h6 class="h6">Technical support</h6>
+                <div class="text">
+                  <p class="">
+                    Your power leans to the technical aspects of ISTE. You
+                    will have over all control over the 404 ISTE webpage. You
+                    will handle the technical side of any event and give
+                    technical assistance to the registration committee when
+                    required. You must assist the technical team head as well.
+                    If you are tech savvy enough, quick board this train!
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+
+          <div class="card-flyer">
+            <div class="text-box">
+              <div class="text-container">
+                <h6 class="h6">Web Team</h6>
+                <div class="text">
+                  <p class="">
+                    You are responsible for all coding works from website development to designing. With ample technical
+                    knowledge and dedication, you will be the right person for the job. The web team is to support ISTE
+                    in web development and provide visual solutions to technical problems. If you have good programming
+                    skills and design approach, feel free to join us.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+          
+        </div>
+        <br /><br />
+        <a href="https://recruitment.istetkmce.in/home.php">
+          <button type="submit" class="btn btn-primary btn-lg btn-block">
+            REGISTER NOW
+          </button>
+        </a>
+      </div>
+      <br />
+    </div>
+    <footer>
+    <div class="footer" id="footer">
+        <div class="container">
+           <div class="row ">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                    <br><br><br>
+                    <img src="./images/logo1.png">
+                </div>
+                <div class="col-lg-4 col-sm-4 col-xs-4">
+                    <h3> Contact </h3>
+                    <ul>
+                        <br/>
+                        <!--<li> <p> Vishnu - 6282520388 </p> </li>-->
+                        <!--<li> <p> Bhagyasree - 9495687277</p> </li>-->
+                        <li>
+                        <p>Fardeen Khan K 7356334434</p>
+                        </li>
+                     <li>
+                        <p>Sreelekshmi N S 8547282695</p>
+                        </li>
+                    </ul>
+                </div>
+               
+               
+            <!--/.row--> 
+        </div>
+        <!--/.container--> 
+    </div>
+    <!--/.footer-->
+                          
+    <div class="footer-bottom">
+        <div class="container">
+           <center><p class=" copyright text-center" style="text-align:center;"> &copy Designed and Developed by 404 ISTE </p></center>
+       
+        </div>
+    </div>
+    <!--/.footer-bottom--> 
+
+</footer>
+  </body>
+</html>
