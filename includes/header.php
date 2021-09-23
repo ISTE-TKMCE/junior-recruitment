f421f98a43ba95142d34e78dc9cf3475fcf4c473
P K Aravindkrishnan <65278075+Aravind-pk@users.noreply.github.com>
2021-09-23 14:37:29 +0530

    init3

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<div class="brand clearfix">
+<h4 class="pull-left text-white text-uppercase" style="margin:20px 0px 0px 20px"><i class=""></i>&nbsp;</h4>
+		<span class="menu-btn"><i class="">
+				<ul>
+					<li><a href="index.php">Home</a>&emsp;<!--<a href="">Register</a>&emsp;--><a href="#footer">Contact</a>&emsp;<a href="./admin/index.php">Admin</a></li>
+				</ul>
+			</i></span>
+		<ul class="ts-profile-nav">
+			
+			<li class="ts-account">
+				<a href="#"><img src="images/29-512.webp" class="ts-avatar hidden-side" alt=""> MENU <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul style="font-size:18px;text-decoration:none;">
+					<li style="font-size:18px;"><a style="text-decoration:none;" href="index.php">Home</a></li>
+					<!--<li style="font-size:18px;"><a href="">Register</a></li>-->
+					<li style="font-size:18px;"><a href="#footer">Contact</a></li>
+					<li style="font-size:18px;"><a href="./admin/index.php">Admin</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+
+ 
---
+++ includes/leftbar.php
@@ -0,0 +1,11 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+					<li><a href="home.php">Home</a></li>
+					<!--<li><a href="index.php">Register</a></li>-->
+					<li><a href="#footer">Contact</a></li>
+					<li><a href="./admin/index.php">Admin</a></li>
+			</ul>
+			<p class="text-center" style="color:#ffffff; margin-top: 100px;">© ISTE</p>
+		</nav>
+
+		
